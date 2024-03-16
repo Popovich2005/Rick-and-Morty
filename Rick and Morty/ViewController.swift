bbbb52599a2c54fe0426a7f7262c7f57486ee9e1
popov <popich2005@mail.ru>
2024-03-16 10:09:33 +0300

    Initial Commit

---
+++ Rick and Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty
+//
+//  Created by Алексей Попов on 16.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
